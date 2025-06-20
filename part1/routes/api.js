430f4f5a1c00e19656fa93b0fea9f97f3b395b6c
Sebastian <sebkruse22@gmail.com>
2025-06-20 06:50:39 +0000

    [automatic commit] 2025-06-20 06:50:39 UTC 103.29.173.247

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -9,7 +9,7 @@ router.get('/dogs', async (req, res) => {
         const dbProm = await adProm;
         const [rows] = await.execute('
             SELECT Dogs.name, Dogs.size, User.username AS owner
-            FROM Dogs JOIN User ON Dogs.owner_id = User.user_id')
+            FROM Dogs JOIN User ON Dogs.owner_id = User.user_id');
             res.json(rows);
     } catch {
         res.json
