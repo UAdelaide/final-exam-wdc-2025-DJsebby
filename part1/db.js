a948b9bade93594ba867e7f669efc44b730ebee9
Sebastian <sebkruse22@gmail.com>
2025-06-20 06:25:42 +0000

    [automatic commit] 2025-06-20 06:25:42 UTC 103.29.173.247

--- part1/db.js
+++ part1/db.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2/promise');
 module.exports = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password
+    password: '',
+    database: 'Dog'
 })
 
