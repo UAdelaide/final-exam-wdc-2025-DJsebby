d6659907b7f0a499b5a0913421b627ae4ae5fc2b
Sebastian <sebkruse22@gmail.com>
2025-06-20 06:38:03 +0000

    [automatic commit] 2025-06-20 06:38:03 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 103.29.173.247

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routee = express.Router();
-const db = require ('..db');
+const dbProm = require ('..db');
 const { router } = require('../app');
 
 router.get('/dogs', async (req, res) => {
     try {
-        const db 
+        const dbProm = await
     } catch {
 
     }
