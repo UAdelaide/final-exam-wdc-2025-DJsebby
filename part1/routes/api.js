33590deb65a77dc55b87228f0a192e48d6789772
Sebastian <sebkruse22@gmail.com>
2025-06-20 06:54:20 +0000

    [automatic commit] 2025-06-20 06:54:20 UTC 103.29.173.247

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -8,8 +8,7 @@ router.get('/dogs', async (req, res) => {
     try {
         const dbProm = await adProm;
         const [rows] = await.execute('
-            SELECT Dogs.name, Dogs.size, User.username AS owner
-            FROM Dogs JOIN User ON Dogs.owner_id = User.user_id');
+            SELECT Dogs.name, Dogs.size, User.username AS owner            FROM Dogs JOIN User ON Dogs.owner_id = User.user_id');
             res.json(rows);
     } catch {
         res.json({Error: 'couldnt fetch dogs route' })
@@ -21,7 +20,8 @@ router.get('/walkrequests/open', async (req, res) => {
         const dbProm = await adProm;
         const [rows] = await.execute('
             SELECT WalkRequests.*, Dogs.name AS dog_name AS
-            FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id');
+            FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+            where');
             res.json(rows);
     } catch {
         res.json({Error: 'couldnt fetch dogs route' })
