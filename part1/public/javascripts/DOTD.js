695ac70c78790f8ce565cf3faaa26a3df3813ae2
Sebastian <sebkruse22@gmail.com>
2025-06-20 08:03:29 +0000

    [automatic commit] 2025-06-20 08:03:29 UTC ;; communications error to 208.67.222.222#53: timed out 103.29.173.247

--- part1/public/javascripts/DOTD.js
+++ part1/public/javascripts/DOTD.js
@@ -10,7 +10,9 @@ createApp({
 
 methods:{
     async loadDog(){
-        const response = await fetch ('https://dog.ceo/api/breeds/image/random')
+        const response = await fetch ('https://dog.ceo/api/breeds/image/random');
+        const res = await response.json();
+        this.imageURL = result
     }
 }
 })
