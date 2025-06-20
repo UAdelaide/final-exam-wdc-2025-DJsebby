951adb3ae4af8ba6270f6fafbbbfcfda32be116a
Sebastian <sebkruse22@gmail.com>
2025-06-20 06:39:44 +0000

    [automatic commit] 2025-06-20 06:39:44 UTC 103.29.173.247

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routee = express.Router();
 const dbProm = require ('..db');
 const { router } = require('../app');
+const db = require('../db');
 
 router.get('/dogs', async (req, res) => {
     try {
         const dbProm = await adProm;
-        const [rows]
+        const [rows] = (await db).execute
     } catch {
 
     }
