e184ae321d395d4608de5388343779404c9cf3ed
Sebastian <sebkruse22@gmail.com>
2025-06-20 07:11:55 +0000

    [automatic commit] 2025-06-20 07:11:55 UTC 103.29.173.247

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -38,7 +38,7 @@ router.get('/walkers/summary', async (req, res) => {
             FROM users
             JOIN WalkApplications ON WalkApplications.walker_id = Users.user_id
             JOIN WalkRequests ON WalkRequests.request_id = WalkApplications.request_id
-            WHERE role = 'walker' AND WalkRequests.status = 'completed' ADN `);
+            WHERE role = 'walker' AND WalkRequests.status = 'completed' AND WalkApplications.status = 'accepted' `);
             res.json(rows);
     } catch {
         res.json({Error: 'couldnt fetch walkrequests route' })
