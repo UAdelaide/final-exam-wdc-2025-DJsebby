938e72c9f9cc7f4f2b9b60e06a67c798fd2e2049
Sebastian <sebkruse22@gmail.com>
2025-06-21 02:21:25 +0000

    [automatic commit] 2025-06-21 02:21:25 UTC 103.29.173.247

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,8 @@ router.post('/login', async (req, res) => {
     if (err) {
       return res.json({ error: 'logout failed in route'})
     }
-    
+    // clear cookie from browser
+    res.clearCookie()
   })
 
 });
