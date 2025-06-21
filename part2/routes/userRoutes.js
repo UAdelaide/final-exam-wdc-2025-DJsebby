347c09ba89d465e43c5c69c88fa5f7a3de99bbb2
Sebastian <sebkruse22@gmail.com>
2025-06-21 02:58:15 +0000

    [automatic commit] 2025-06-21 02:58:15 UTC 103.29.173.247

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -76,7 +76,9 @@ router.post('/logout', async (req, res) => {
 
   router.get(/dogsIDs, async (req,res) =>{
     // check if user is logged in first with tthe cookie
-    if (!req.session.user)
+    if (!req.session.user){
+      return res.json
+    }
   })
 
 });
