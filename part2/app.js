c7a012a95040aec47121bd77c7abccf6405a8ed0
Sebastian <sebkruse22@gmail.com>
2025-06-21 05:16:17 +0000

    [automatic commit] 2025-06-21 05:16:17 UTC 103.29.173.247

--- part2/app.js
+++ part2/app.js
@@ -23,7 +23,7 @@ const dogRoutes = require('../part1/routes/api.js');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/dogs', )
+app.use('/api/dogs', dogRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
