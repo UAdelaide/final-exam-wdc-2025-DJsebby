45c34fc487c2dcf0f57234aa9b2f513d781152a1
Sebastian <sebkruse22@gmail.com>
2025-06-21 05:14:36 +0000

    [automatic commit] 2025-06-21 05:14:36 UTC 103.29.173.247

--- part2/app.js
+++ part2/app.js
@@ -19,6 +19,7 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
